a93532147aaeba5c0db5549632a2649e0643e1c9
Lio <gomezlionelsebastian@gmail.com>
2025-10-14 22:33:10 -0300

    Integración inicial de EJS en index, creación de partials

---
+++ data.js
@@ -0,0 +1,14 @@
+module.exports = {
+  banners: [
+    { image: "banner.jpg", alt: "Nueva colección Marcela Koury" },
+    { image: "banner2.jpg", alt: "Promo día de la madre" },
+    { image: "banner3.jpg", alt: "Talles especiales" }
+  ],
+  brands: [
+    { name: "marcelaKoury", image: "marcelaKoury.jpg" },
+    { name: "sigry", image: "sigry.jpg" },
+    { name: "kaury", image: "kaury.png" },
+    { name: "natubel", image: "natubel.jpg" },
+    { name: "lody", image: "lody.png" }
+  ]
+};
