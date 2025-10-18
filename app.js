1fd21d79098e33e3a64a90c4dff984379edd8a4d
Lio <gomezlionelsebastian@gmail.com>
2025-10-18 00:08:56 -0300

    Se agrega tab precaria

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+///////////// Require's ///////////////////
 const express = require('express');
-const app = express();
 const path = require('path');
-
+const data = require('./data.js');
+///////////// express() ///////////////////
+const app = express();
+///////////// Middleware //////////////////
 app.use(express.static(path.join(__dirname, 'public')));
+///////////// Template engine /////////////
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-const data = require('./data.js');
-
+////////////////////////////////////////
 app.get('/', (req, res) => {
   res.render('index', { banners: data.banners, brands: data.brands });
 });
-
+app.get('/brands', (req, res) => {
+    res.render('brands', { brands: data.brands });
+});
+///// Listen URL + console log ////////
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
+////////////////////////////////////////
